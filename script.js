a9d043678225976e14f30677142f14787556bb2c
Alisha Sojar <alisha.sojar@gmail.com>
2020-02-08 00:00:24 +0000

    v3

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 function printFunction() {
   window.print();
 }
-console.log("Pizza");
+
+
 $('.submitButton').click(function(){
-  console.log('hi');
-  var thing = $('.descriptionText').val();
-  $('.description').html(thing);
-  console.log(thing);
-});
-$('.imageoption').click(function(){
-  
- // var thing = $('.descriptionText').val();
- // $('.description').html(thing);
-  console.log('clciekd me');
+ 
+
+  $(".displayBlog").hide()
+  $(".displayPage").show()
 });
 
+
+$('#img1').click(function(){
+  
+ var imgSrc = $('#img1').attr('src')
+ $('.imageOption').removeClass('showBorder')
+ $('#img1').addClass('showBorder')
+ $('.posterImage').attr('src', imgSrc)
+ // var thing = $('.descriptionText').val();
+ // $('.description').html(thing);
+});
+
+$('#img2').click(function(){
+  
+ var imgSrc = $('#img1').attr('src')
+ $('.imageOption').removeClass('showBorder')
+ $('#img1').addClass('showBorder')
+ $('.posterImage').attr('src', imgSrc)
+ // var thing = $('.descriptionText').val();
+ // $('.description').html(thing);
+});
+
+$('#img3').click(function(){
+  
+ var imgSrc = $('#img1').attr('src')
+ $('.imageOption').removeClass('showBorder')
+ $('#img1').addClass('showBorder')
+ $('.posterImage').attr('src', imgSrc)
+ // var thing = $('.descriptionText').val();
+ // $('.description').html(thing);
+});
+
+$('#img4').click(function(){
+  
+ var imgSrc = $('#img1').attr('src')
+ $('.imageOption').removeClass('showBorder')
+ $('#img1').addClass('showBorder')
+ $('.posterImage').attr('src', imgSrc)
+ // var thing = $('.descriptionText').val();
+ // $('.description').html(thing);
+});
+
+
+
 function text() {
-  var x = document.getElementById("mySelect").value;
-  document.getElementById("demo").innerHTML = "You selected: " + x;
+  var x = $("#topic").val();
+  window.location.href="/blog.html?topic=" + x
 }
